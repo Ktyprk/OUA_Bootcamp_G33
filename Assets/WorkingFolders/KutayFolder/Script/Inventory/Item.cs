6dd0358b8e23be3adcb640ec7b33864ac8eff60a
Ktyprk <Kutayperk@hotmail.com>
2024-06-21 20:53:48 +0300

    InventoryStarting

---
+++ Assets/WorkingFolders/KutayFolder/Script/Inventory/Item.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemIcon;
+    public int itemID;
+    public string itemDescription;
+    public int itemValue;
+    public int itemDamage;
+    public int itemArmor;
+    public ItemType itemType;
+    
+    public enum ItemType
+    {
+        Weapon,
+        Armor,
+        Consumable,
+        Quest,
+        Money
+    }
+    public Item(string name, Sprite icon, int id, string description, int value, int damage, int armor, ItemType type)
+    {
+        itemName = name;
+        itemIcon = icon;
+        itemID = id;
+        itemDescription = description;
+        itemValue = value;
+        itemDamage = damage;
+        itemArmor = armor;
+        itemType = type;
+    }
+    
+    public Item(Item item)
+    {
+        itemName = item.itemName;
+        itemIcon = item.itemIcon;
+        itemID = item.itemID;
+        itemDescription = item.itemDescription;
+        itemValue = item.itemValue;
+        itemDamage = item.itemDamage;
+        itemArmor = item.itemArmor;
+        itemType = item.itemType;
+    }
+    
+}
