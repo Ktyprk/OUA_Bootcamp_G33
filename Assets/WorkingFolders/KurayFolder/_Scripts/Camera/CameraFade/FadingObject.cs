005989c58dafa168b876935078dc9c6727062683
KurayTunc <kuraytunc@outlook.com>
2024-06-26 14:16:32 +0300

    Camera&Car&Outline

---
+++ Assets/WorkingFolders/KurayFolder/_Scripts/Camera/CameraFade/FadingObject.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Kaybolmasýný istediðimiz objelere verilen script
+/// </summary>
+public class FadingObject : MonoBehaviour, IEquatable<FadingObject>
+{
+    public List<Renderer> Renderers = new List<Renderer>();
+    public Vector3 Position;
+    public List<Material> Materials = new List<Material>();
+    [HideInInspector]
+    public float InitialAlpha;
+
+    private void Awake()
+    {
+        Position = transform.position;
+
+        if (Renderers.Count == 0)
+        {
+            Renderers.AddRange(GetComponentsInChildren<Renderer>());
+        }
+        foreach (Renderer renderer in Renderers)
+        {
+            Materials.AddRange(renderer.materials);
+        }
+
+        InitialAlpha = Materials[0].color.a;
+    }
+
+    public bool Equals(FadingObject other)
+    {
+        return Position.Equals(other.Position);
+    }
+
+    public override int GetHashCode()
+    {
+        return Position.GetHashCode();
+    }
+}
---
+++ Assets/WorkingFolders/KurayFolder/_Scripts/DialogueSystem/ConversationStarter.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+using DialogueEditor;
+
+public class ConversationStarter : MonoBehaviour
+{
+    [SerializeField] private NPCConversation myConversation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+            }
+        }
+    }
+}
