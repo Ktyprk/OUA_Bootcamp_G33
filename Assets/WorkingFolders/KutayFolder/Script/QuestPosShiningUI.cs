dd6239474d837a748b43fe903511107ef46e28dd
Ktyprk <Kutayperk@hotmail.com>
2024-08-01 20:37:35 +0300

    ui updated

--- Assets/WorkingFolders/KutayFolder/Script/QuestPosShiningUI.cs
+++ Assets/WorkingFolders/KutayFolder/Script/QuestPosShiningUI.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class QuestPosShiningUI : MonoBehaviour
 {
-    private static  QuestPosShiningUI instance;
+    private static QuestPosShiningUI instance;
     
     private Image image;
 
@@ -14,6 +14,8 @@ public class QuestPosShiningUI : MonoBehaviour
     [SerializeField] private Transform[] waypoints;
     private int currentWaypointIndex;
     private Transform waypoint => waypoints[currentWaypointIndex];
+    
+    [SerializeField] private Transform character; // Reference to the character's Transform
 
     private void Start()
     {
@@ -22,40 +24,34 @@ public class QuestPosShiningUI : MonoBehaviour
 
     private void Update()
     {
-        
-            Vector2 position = Camera.main.WorldToScreenPoint(waypoint.position);
+        Vector2 position = Camera.main.WorldToScreenPoint(waypoint.position);
 
-            position.x = Mathf.Clamp(position.x, 0, Screen.width);
-            position.y = Mathf.Clamp(position.y, 0, Screen.height);
+        position.x = Mathf.Clamp(position.x, 0, Screen.width);
+        position.y = Mathf.Clamp(position.y, 0, Screen.height);
 
-            if(Vector3.Dot((waypoint.position - Camera.main.transform.position), Camera.main.transform.forward) < 0)
+        if (Vector3.Dot((waypoint.position - Camera.main.transform.position), Camera.main.transform.forward) < 0)
+        {
+            position.x = Mathf.Abs(Screen.width - position.x);
+            position.y = Mathf.Abs(Screen.height - position.y);
+
+            if ((position.x != 0 && position.x != Screen.width) && (position.y != 0 && position.y != Screen.height))
             {
-                position.x = Mathf.Abs(Screen.width - position.x);
-                position.y = Mathf.Abs(Screen.height - position.y);
-
-                if ((position.x != 0 && position.x != Screen.width) && (position.y != 0 && position.y != Screen.height))
+                if (position.x < Screen.width / 2)
                 {
-                    if(position.x < Screen.width / 2)
-                    {
-                        position.x = 0;
-                    }
-                    else
-                    {
-                        position.x = Screen.width;
-                    }
+                    position.x = 0;
+                }
+                else
+                {
+                    position.x = Screen.width;
                 }
             }
+        }
 
-            if((position.x != 0 && position.x != Screen.width) && (position.y != 0 && position.y != Screen.height))
-            {
-                image.enabled = false;
-            }
-            else
-            {
-                if (!image.enabled) image.enabled = true;
-            
-                transform.position = position;
-            }
-        
+        float distance = Vector3.Distance(character.position, waypoint.position);
+
+        if (distance < 5f)
+        {
+            image.enabled = false;
+        }
     }
 }
